1230cd97eecdd616fb2bf3a7171cdffb9649e33c
TerickD <TerickMDupont@gmail.com>
2025-09-09 20:03:53 -0400

    landing page and nav bar complete

--- models/user.js
+++ models/user.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
+
+const sightingSchema = new mongoose.Schema({
+  date: {
+    type: Number,
+    required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    enum: [    "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+    "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+    "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"],
+    required: true
+  },
+  comments: {
+    type: String, 
+  },
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +38,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  sightings: [sightingSchema],
 });
 
 const User = mongoose.model('User', userSchema);
