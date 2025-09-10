0cbbb0bde05f45731d26811c60e4cc6e5f09f726
TerickD <TerickMDupont@gmail.com>
2025-09-10 19:49:02 -0400

    create form inputs complete possible blocker

--- controllers/applications.js
+++ controllers/applications.js
@@ -12,4 +12,20 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.sightings.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
