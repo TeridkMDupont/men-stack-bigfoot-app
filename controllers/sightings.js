b9d272ac349e3c435a939dd305ec0ca12339593e
TerickD <TerickMDupont@gmail.com>
2025-09-14 14:43:03 -0400

    all CRUD functions complete

--- controllers/sightings.js
+++ controllers/sightings.js
@@ -5,7 +5,10 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('sightings/index.ejs');
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('sightings/index.ejs', {
+      sightings: currentUser.sightings,
+    });
   } catch (error) {
     console.log(error);
     res.redirect('/');
@@ -20,6 +23,65 @@ router.post('/', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     currentUser.sightings.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/sightings`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+//Show Page
+router.get('/:sightingId', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the application by the applicationId supplied from req.params
+    const sightings = currentUser.sightings.id(req.params.sightingId);
+    // Render the show view, passing the application data in the context object
+    res.render('sightings/show.ejs', {
+      sighting: sightings,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+
+router.get('/:sightingId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const sightings = currentUser.sightings.id(req.params.sightingId);
+    res.render('sightings/edit.ejs', {
+      sighting: sightings,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.put('/:sightingId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const sighting = currentUser.sightings.id(req.params.sightingId);
+    sighting.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/sightings/${req.params.sightingId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+
+router.delete('/:sightingId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.sightings.id(req.params.sightingId).deleteOne();
     await currentUser.save();
     res.redirect(`/users/${currentUser._id}/sightings`);
   } catch (error) {
